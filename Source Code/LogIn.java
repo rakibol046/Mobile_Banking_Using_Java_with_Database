6d1fdecb821bd34e8560b68fee142cf18b718e3d
Md Rakibul Islam <43632702+rakibol046@users.noreply.github.com>
2021-07-25 18:34:47 +0600

    Update LogIn.java

--- Source Code/LogIn.java
+++ Source Code/LogIn.java
@@ -18,49 +18,45 @@ import java.util.Scanner;
  * @author Md. Rakibul Islam
  */
 public class LogIn {
-    Connection dbconnection;
-    public Scanner scanner = new Scanner(System.in);
-    String connectionUrl = "jdbc:mysql://127.0.0.1/bankcustomer";
-    
-    void log() throws ClassNotFoundException, SQLException{
-          try {
-            // int id
-            System.out.println("Enter Phone : ");
-            String phone = scanner.next();
-            System.out.println("Enter Password : ");
-            String password = scanner.next();
-            try {
-                dbconnection = DriverManager.getConnection(connectionUrl, "root", "");
-                // create a statement object to send to database
-                String checkQuery = "select * from customer where phone = ? AND pass= ?  ";
-                // prepare all data before insert it
-                try (PreparedStatement preparedStatement = dbconnection.prepareStatement(checkQuery)) {
-                    // prepare all data before insert it
-                    preparedStatement.setString(1, phone);
-                    preparedStatement.setString(2, password);
-                   
-                    // return 0 if not query compete  Or 1 if not
-                    ResultSet result = preparedStatement.executeQuery();
-                    
-                    if (result.next())
-                    {
-                         String name = result.getString("name");
-                         int balance = result.getInt("balance");
-                         Banking bank = new Banking(name, phone, balance);                       
+  Connection dbconnection;
+  public Scanner scanner = new Scanner(System.in);
+  String connectionUrl = "jdbc:mysql://127.0.0.1/bankcustomer";
 
-                        
-                    }else
-                    {
-                        System.out.println("Password incorrect ");
-                        
-                    }
-                }
-            } catch (SQLException throwables) {
-                System.out.println("error from database   ");
-            }
-        } catch (Exception e) {
-            System.out.println("Sorry Input error ");
+  void log() throws ClassNotFoundException, SQLException {
+    try {
+
+      System.out.println("Enter Phone : ");
+      String phone = scanner.next();
+      System.out.println("Enter Password : ");
+      String password = scanner.next();
+      try {
+        dbconnection = DriverManager.getConnection(connectionUrl, "root", "");
+
+        String checkQuery = "select * from customer where phone = ? AND pass= ?  ";
+
+        try (PreparedStatement preparedStatement = dbconnection.prepareStatement(checkQuery)) {
+
+          preparedStatement.setString(1, phone);
+          preparedStatement.setString(2, password);
+
+          ResultSet result = preparedStatement.executeQuery();
+
+          if (result.next()) {
+            String name = result.getString("name");
+            int balance = result.getInt("balance");
+            Banking bank = new Banking(name, phone, balance);
+
+          } else {
+            System.out.println("Password incorrect ");
+
+          }
         }
+      } catch (SQLException throwables) {
+        System.out.println("error from database   ");
+      }
+    } catch (Exception e) {
+      System.out.println("Sorry Input error ");
     }
-    
+  }
+
 }
